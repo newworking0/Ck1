837ea669775cbcf8b2ea47b58a3da501a66ca2c1
newworking0 <supervpn290@gmail.com>
2025-05-18 15:36:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -232,14 +232,8 @@ async def main():
 
 if __name__ == '__main__':
     import asyncio
-    try:
-        asyncio.run(main())
-    except RuntimeError as e:
-        if "already running" in str(e):
-            import nest_asyncio
-            nest_asyncio.apply()
-            loop = asyncio.get_event_loop()
-            loop.create_task(main())
-            loop.run_forever()
-        else:
-            raise e
+    import nest_asyncio
+
+    nest_asyncio.apply()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
